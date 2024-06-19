297c91e48dc245e2ddc22e0f25d2e8642e0e92f8
Caio Fernando <caiofsr@proton.me>
2024-06-19 15:50:18 -0300

    improve logging

--- src/serial/serial-handler.service.ts
+++ src/serial/serial-handler.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { SerialPort } from 'serialport';
 
 @Injectable()
 export class SerialHandlerService {
   public serialport: SerialPort;
+  private logger: Logger;
 
   constructor() {
+    this.logger = new Logger();
     this.serialport = new SerialPort({
       path: 'COM3',
       baudRate: 9600,
     });
 
     this.serialport.on('open', () => {
-      console.log('Serial port opened');
+      this.logger.log('Serial port connected', 'SerialModule');
     });
   }
 }
